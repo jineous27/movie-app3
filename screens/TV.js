d83d6c7ebc311c102a22f8d7b4c8fb9e7bf1f574
jinheeyi <jinhee.yi27@gmail.com>
2020-02-06 07:15:14 +1000

    Add app screens

---
+++ screens/TV.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class TV extends Component {
+    render() {
+        return (
+            <View>
+                <Text>TV</Text>
+            </View>            
+        );
+    }
+}
